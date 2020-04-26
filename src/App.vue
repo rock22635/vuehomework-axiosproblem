bbe6a8781cc22c9ee3248a844bab087c920d7c1d
rock22635 <rock014124@gmail.com>
2020-04-26 19:25:50 +0800

    vueproblem
    vueproblem

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Vue from 'vue'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+Vue.use(axios,VueAxios);
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  created() {
+    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`; // 'http://localhost:3000/api/casper/products';
+    // API 伺服器路徑
+    // 所申請的 APIPath
+    console.log(process.env.APIPATH, process.env.CUSTOMPATH);
+    this.$http.get(api).then((response) => {
+      console.log(response.data);
+    });
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import App from './App'
+
+Vue.config.productionTip = false
+Vue.use(axios,VueAxios)
+//Vue.prototype.$http = axios
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
